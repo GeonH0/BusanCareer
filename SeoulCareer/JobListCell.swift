b148dc8a287c8894fbf1258a62dc2ad6cd84376c
GeonH0 <h2jo369@naver.com>
2023-09-20 09:04:39 +0900

    SetTableCell

---
+++ SeoulCareer/JobListCell.swift
@@ -0,0 +1,23 @@
+//
+//  JobListCell.swift
+//  SeoulCareer
+//
+//  Created by 김건호 on 2023/09/19.
+//
+
+import UIKit
+
+class JobListCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ SeoulCareer/ListViewContoroller.swift
@@ -0,0 +1,11 @@
+//
+//  ListViewContoroller.swift
+//  SeoulCareer
+//
+//  Created by 김건호 on 2023/09/19.
+//
+
+import Foundation
+import Alamofire
+
+class ListViewController : UIVIE
