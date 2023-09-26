fb4a1d56241307544bd4f982c451bb0208e3aa93
GeonH0 <h2jo369@naver.com>
2023-09-26 17:09:45 +0900

    Set Json

--- SeoulCareer/Jobs.swift
+++ SeoulCareer/Jobs.swift
@@ -7,77 +7,46 @@
 
 import Foundation
 
-//
-//struct getJobOpnngInfo: Codable {
-//    struct Header: Codable {
-//        let resultCode: String
-//        let resultMsg: String
-//    }
-//
-//    struct JobOpening: Codable {
-//        let title: String
-//        let recruitAgencyName: String
-//        let recruitAgencyType : String
-//        let mngDept : String
-//        let mngName : String
-//        let bunya: String
-//        let workDateType: String
-//        let workDate_nm : String
-//        let workregiontxt : String
-//        let reqDate_s: String
-//        let reqDate_e: String
-//        let reqType: String
-//        let reqType_nm: String
-//        let regDate: String
-//        let modDate: String
-//    }
-//
-//    struct Body: Codable {
-//        let items: Items
-//        let numOfRows: Int
-//        let pageNo: Int
-//        let totalCount: Int
-//    }
-//
-//    struct Items: Codable {
-//        let item: [JobOpening]
-//    }
-//
-//    let header: Header
-//    let body: Body
-//}
-
+struct JobOpnngInfoResponse : Codable {
+      let getJobOpnngInfo : Body
+}
 
 struct JobInfo: Codable {
     let title: String
     let recruitAgencyName: String
-    let recruitAgencyType : String
-    let mngDept : String
-    let mngName : String
+    let recruitAgencyType: String
+    let mngDept: String
+    let mngName: String
     let bunya: String
-    let workDateType: String
-    let workDate_nm : String
-    let workregiontxt : String
-    let reqDate_s: String
-    let reqDate_e: String
-    let reqType: String
-    let reqType_nm: String
-    let regDate: String
-    let modDate: String
+    let workDate_type: String
+    let workDate_nm: String
+    let workregiontxt: String?
+    let reqDate_s: String?
+    let reqDate_e: String?
+    let reqType:String
+    let reqType_nm:String
+    let regDate:String
+    let modDate:String
 }
 
-struct Item: Codable {
-    let item: [JobInfo]
+struct Item : Codable {
+    var item : [JobInfo]?
 }
 
-struct Items: Codable {
-    let items: Item
+struct Items : Codable {
+    let items : Item
 }
 
-struct Body: Codable {
-    let body: Items
+struct Body : Codable {
+    var numOfRows:Int?
+    var pageNo:Int?
+    var totalCount:Int?
+    var body : Items
 }
 
-struct JobOpnngInfoResponse : Codable {
-   let getJobOpnngInfo : Body
+struct Header:Codable{
+  var resultCode:String?
+  var resultMsg:String?
 }
+  
+
