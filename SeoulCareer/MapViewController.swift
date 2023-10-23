8babd6493aa110847657d60e7a39dfee5177baaa
GeonH0 <h2jo369@naver.com>
2023-10-23 10:02:15 +0900

    현재위치 활성화

--- SeoulCareer/MapViewController.swift
+++ SeoulCareer/MapViewController.swift
@@ -1,21 +1,28 @@
-//
-//  MapViewController.swift
-//  SeoulCareer
-//
-//  Created by 김건호 on 2023/09/12.
-//
-
 import UIKit
 import NMapsMap
 import CoreLocation
 
-class MapViewController: UIViewController,CLLocationManagerDelegate {
-    
-    var locationManager = CLLocationManager()
-    
+class MapViewController: UIViewController, CLLocationManagerDelegate {
+
+    var locationManager: CLLocationManager!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+
+        // 요청하는 위치 서비스 권한을 선택합니다.
+        locationManager.requestWhenInUseAuthorization() // 또는 locationManager.requestAlwaysAuthorization()
+
+        if CLLocationManager.locationServicesEnabled() {
+            print("위치 서비스 On")
+            locationManager.startUpdatingLocation()
+        } else {
+            print("위치 서비스 Off 상태")
+        }
+
         let mapNaverView = NMFNaverMapView()
         mapNaverView.showZoomControls = false
         mapNaverView.showLocationButton = true
@@ -23,35 +30,17 @@ class MapViewController: UIViewController,CLLocationManagerDelegate {
         mapNaverView.mapView.isTiltGestureEnabled = true
         mapNaverView.mapView.isRotateGestureEnabled = true
         mapNaverView.mapView.isStopGestureEnabled = true
-        
-//        // Auto Layout 사용을 위해 필요함.
+
+        // Auto Layout 사용을 위해 필요함.
         mapNaverView.translatesAutoresizingMaskIntoConstraints = false
-        
+
         view.addSubview(mapNaverView)
-        
-//        locationManager.delegate = self
-//                locationManager.desiredAccuracy = kCLLocationAccuracyBest
-//                locationManager.requestWhenInUseAuthorization()
-//        
-//        if CLLocationManager.locationServicesEnabled() {
-//            printContent("위치 서비스 On")
-//            locationManager.startUpdatingLocation()
-//            
-//            print(locationManager.location?.coordinate)
-//                } else {
-//                        print("위치 서비스 Off 상태")
-//                    }
-        
-        
-        
-       NSLayoutConstraint.activate([
-        mapNaverView.topAnchor.constraint(equalTo: view.topAnchor),
-        mapNaverView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
-        mapNaverView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-        mapNaverView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
-       ])
+
+        NSLayoutConstraint.activate([
+            mapNaverView.topAnchor.constraint(equalTo: view.topAnchor),
+            mapNaverView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            mapNaverView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            mapNaverView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        ])
     }
-    
-    
-    
 }
