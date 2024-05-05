ad4891538fce186d8cacb81d6547b5615c9481f6
GeonH0 <h2jo369@naver.com>
2024-05-05 22:25:30 +0900

    [Refactor]: JobDetailView layout 수정

--- BusanCareer/JobDetailVIew/JobDetailView.swift
+++ BusanCareer/JobDetailVIew/JobDetailView.swift
@@ -11,6 +11,7 @@ import UIKit
 class JobDetailView: UIView {
     
     private let jobTitleLabel = UILabel()
+    private let stackView = UIStackView()
     private let agencyNameLabel = UILabel()
     private let bunyaLabel = UILabel()
     private let endDateLabel = UILabel()
@@ -19,60 +20,67 @@ class JobDetailView: UIView {
     override init(frame: CGRect) {
         super.init(frame: frame)
         backgroundColor = .white
-        setupViews()
+        setupLabels()
+        setupStackView()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
 
-    private func setupViews() {
-        addSubview(jobTitleLabel)
-        addSubview(agencyNameLabel)
-        addSubview(bunyaLabel)
-        addSubview(endDateLabel)
-        addSubview(reqTypeLabel)
+    private func setupLabels() {
+        jobTitleLabel.text = "Job Title"
+        agencyNameLabel.text = "Agency Name"
+        bunyaLabel.text = "Field"
+        endDateLabel.text = "End Date"
+        reqTypeLabel.text = "Requirement Type"
 
-        jobTitleLabel.translatesAutoresizingMaskIntoConstraints = false
-        agencyNameLabel.translatesAutoresizingMaskIntoConstraints = false
-        bunyaLabel.translatesAutoresizingMaskIntoConstraints = false
-        endDateLabel.translatesAutoresizingMaskIntoConstraints = false
-        reqTypeLabel.translatesAutoresizingMaskIntoConstraints = false
-
-        setupLayout()
+        [jobTitleLabel, agencyNameLabel, bunyaLabel, endDateLabel, reqTypeLabel].forEach {
+            $0.translatesAutoresizingMaskIntoConstraints = false
+            $0.textAlignment = .center
+        }
+        
+        jobTitleLabel.numberOfLines = 2
+        
     }
 
-    private func setupLayout() {
+
+    private func setupStackView() {
+        stackView.axis = .vertical
+        stackView.distribution = .fillEqually
+        stackView.alignment = .leading
+        stackView.spacing = 5
+        
+        [agencyNameLabel, reqTypeLabel, endDateLabel, bunyaLabel].forEach {
+            stackView.addArrangedSubview($0)
+            $0.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        }
+
+        addSubview(jobTitleLabel)
+        addSubview(stackView)
+        
+        jobTitleLabel.translatesAutoresizingMaskIntoConstraints = false
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        
         NSLayoutConstraint.activate([
-            jobTitleLabel.topAnchor.constraint(equalTo: self.topAnchor, constant: 20),
-            jobTitleLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 20),
-            jobTitleLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -20),
-
-            agencyNameLabel.topAnchor.constraint(equalTo: jobTitleLabel.bottomAnchor, constant: 10),
-            agencyNameLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 20),
-            agencyNameLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -20),
-
-            reqTypeLabel.topAnchor.constraint(equalTo: agencyNameLabel.bottomAnchor, constant: 10),
-            reqTypeLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 20),
-            reqTypeLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -20),
-
-            endDateLabel.topAnchor.constraint(equalTo: reqTypeLabel.bottomAnchor, constant: 10),
-            endDateLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 20),
-            endDateLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -20),
-
-            bunyaLabel.topAnchor.constraint(equalTo: endDateLabel.bottomAnchor, constant: 10),
-            bunyaLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 20),
-            bunyaLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -20),
-            bunyaLabel.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -20)
+            jobTitleLabel.topAnchor.constraint(equalTo: self.topAnchor, constant: 40),
+            jobTitleLabel.centerXAnchor.constraint(equalTo: self.centerXAnchor),
+            jobTitleLabel.leadingAnchor.constraint(greaterThanOrEqualTo: self.leadingAnchor, constant: 20),
+            jobTitleLabel.trailingAnchor.constraint(lessThanOrEqualTo: self.trailingAnchor, constant: -20),
+            jobTitleLabel.bottomAnchor.constraint(equalTo: stackView.topAnchor, constant: -20),
+            
+            stackView.topAnchor.constraint(equalTo: jobTitleLabel.bottomAnchor, constant: 20),
+            stackView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 20),
+            stackView.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -20),
+            stackView.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -150)
         ])
     }
     
     func configure(with job: JobItem) {
-        jobTitleLabel.text = job.title
-        agencyNameLabel.text = job.recruitAgencyName
-        reqTypeLabel.text = job.reqTypeNm
-        endDateLabel.text = job.reqDateE
-        bunyaLabel.text = job.bunya?.isEmpty ?? true ? "분야 없음" : job.bunya
+        jobTitleLabel.text = "공고: " + job.title
+        agencyNameLabel.text = "모집기관: " + job.recruitAgencyName
+        reqTypeLabel.text = "접수 방법: " + (job.reqTypeNm ?? "정보 없음")
+        endDateLabel.text = "마감일: " + (job.reqDateE ?? "정보 없음")
+        bunyaLabel.text = "모집 분야: " + ((job.bunya?.isEmpty ?? true ? "분야 없음" : job.bunya) ?? "정보 없음")
     }
 }
-
